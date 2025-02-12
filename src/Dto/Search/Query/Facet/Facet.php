629c6f90ac7e86bfd1bda646910abbaf3392b36d
Felix Becker <becker@sitepark.com>
2025-02-12 10:40:06 +0100

    feat: added SearchQueryDenormalizer

--- src/Dto/Search/Query/Facet/Facet.php
+++ src/Dto/Search/Query/Facet/Facet.php
@@ -4,9 +4,28 @@ declare(strict_types=1);
 
 namespace Atoolo\Search\Dto\Search\Query\Facet;
 
+use Symfony\Component\Serializer\Attribute\DiscriminatorMap;
+
 /**
  * @codeCoverageIgnore
  */
+#[DiscriminatorMap(
+    typeProperty: 'type',
+    mapping: [
+        'absoluteDateRange' => AbsoluteDateRangeFacet::class,
+        'category' => CategoryFacet::class,
+        'contentSectionType' => ContentSectionTypeFacet::class,
+        'contentType' => ContentTypeFacet::class,
+        'group' => GroupFacet::class,
+        'multiQuery' => MultiQueryFacet::class,
+        'objectType' => ObjectTypeFacet::class,
+        'query' => QueryFacet::class,
+        'relativeDateRange' => RelativeDateRangeFacet::class,
+        'site' => SiteFacet::class,
+        'source' => SourceFacet::class,
+        'spatialDistanceRange' => SpatialDistanceRangeFacet::class,
+    ],
+)]
 abstract class Facet
 {
     /**
--- src/Dto/Search/Query/Filter/Filter.php
+++ src/Dto/Search/Query/Filter/Filter.php
@@ -4,9 +4,33 @@ declare(strict_types=1);
 
 namespace Atoolo\Search\Dto\Search\Query\Filter;
 
+use Symfony\Component\Serializer\Attribute\DiscriminatorMap;
+
 /**
  * @codeCoverageIgnore
  */
+#[DiscriminatorMap(
+    typeProperty: 'type',
+    mapping: [
+        'absoluteDateRange' => AbsoluteDateRangeFilter::class,
+        'and' => AndFilter::class,
+        'category' => CategoryFilter::class,
+        'contentSectionType' => ContentSectionTypeFilter::class,
+        'contentType' => ContentTypeFilter::class,
+        'geoLocated' => GeoLocatedFilter::class,
+        'group' => GroupFilter::class,
+        'id' => IdFilter::class,
+        'not' => NotFilter::class,
+        'objectType' => ObjectTypeFilter::class,
+        'or' => OrFilter::class,
+        'query' => QueryFilter::class,
+        'relativeDateRange' => RelativeDateRangeFilter::class,
+        'site' => SiteFilter::class,
+        'source' => SourceFilter::class,
+        'spatialArbitraryRectangle' => SpatialArbitraryRectangleFilter::class,
+        'spatialOrbital' => SpatialOrbitalFilter::class,
+    ],
+)]
 abstract class Filter
 {
     /**
