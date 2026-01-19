c1cbd3db5f0c9cb62679d7d7b8de20c76f9afd16
Felix Becker <becker@sitepark.com>
2026-01-19 11:53:08 +0100

    feat: added new meta fields to IndexSchema2xDocument

--- test/Service/Indexer/IndexSchema2xDocumentTest.php
+++ test/Service/Indexer/IndexSchema2xDocumentTest.php
@@ -23,6 +23,78 @@ class IndexSchema2xDocumentTest extends TestCase
         );
     }
 
+    public function testSetMetaInt(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaInt('myname', 42);
+
+        $this->assertEquals(
+            ['sp_meta_int_myname' => 42],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaSingleInt(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaSingleInt('myname', 42);
+
+        $this->assertEquals(
+            ['sp_meta_single_int_myname' => 42],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaLong(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaLong('myname', 42);
+
+        $this->assertEquals(
+            ['sp_meta_long_myname' => 42],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaSingleLong(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaSingleLong('myname', 42);
+
+        $this->assertEquals(
+            ['sp_meta_single_long_myname' => 42],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaFloat(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaFloat('myname', 4.2);
+
+        $this->assertEquals(
+            ['sp_meta_float_myname' => 4.2],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaSingleFloat(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaSingleFloat('myname', 4.2);
+
+        $this->assertEquals(
+            ['sp_meta_single_float_myname' => 4.2],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
     public function testSetMetaString(): void
     {
         $doc = new IndexSchema2xDocument();
@@ -30,6 +102,18 @@ class IndexSchema2xDocumentTest extends TestCase
 
         $this->assertEquals(
             ['sp_meta_string_myname' => 'myvalue'],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaSingleString(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaSingleString('myname', 'myvalue');
+
+        $this->assertEquals(
+            ['sp_meta_single_string_myname' => 'myvalue'],
             $doc->getFields(),
             'unexpected meta fields',
         );
@@ -47,6 +131,18 @@ class IndexSchema2xDocumentTest extends TestCase
         );
     }
 
+    public function testSetMetaSingleText(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaSingleText('myname', 'myvalue');
+
+        $this->assertEquals(
+            ['sp_meta_single_text_myname' => 'myvalue'],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
     public function testSetMetaBool(): void
     {
         $doc = new IndexSchema2xDocument();
@@ -54,6 +150,18 @@ class IndexSchema2xDocumentTest extends TestCase
 
         $this->assertEquals(
             ['sp_meta_bool_myname' => true],
+            $doc->getFields(),
+            'unexpected meta fields',
+        );
+    }
+
+    public function testSetMetaSingleBool(): void
+    {
+        $doc = new IndexSchema2xDocument();
+        $doc->setMetaSingleBool('myname', true);
+
+        $this->assertEquals(
+            ['sp_meta_single_bool_myname' => true],
             $doc->getFields(),
             'unexpected meta fields',
         );
